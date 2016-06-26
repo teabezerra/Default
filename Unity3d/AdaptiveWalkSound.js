dc1e69398dd7690578f853f103423d7750a7e02e
teabezerra <thales.eab@gmail.com>
2016-06-25 23:00:14 -0300

    Primeira publicação
    Script unity3d em JavaScript para controle de audio de passos de
    personagens em terrenos variados e com maior imersão de cena.

---
+++ Unity3d/AdaptiveWalkSound.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+
+// Pequeno script para controle de audio de passos em diversos terrenos
+// Permite alternar dinamicamente o audio clip para cada tipo de terreno
+// Cria uma peguena variação de pitch, dando maior sensação de imersão de cena
+
+
+var audioclip_terra: AudioClip;
+var audioclip_agua: AudioClip;
+
+
+function OnTriggerEnter (tr : Collider)
+{
+
+	var audioclip_temp: AudioClip;
+
+	if(tr.gameObject.name == "Terrain"){
+		
+		// Variação de sonoridade
+	 	audio.pitch = Random.Range(1.35f, 1.5f);
+		
+		// audio de passos na terra
+		audioclip_temp = audioclip_terra;
+		
+ 	}
+
+	else if(tr.gameObject.name == "water"){ // Caso a 
+		
+		// Variação de sonoridade
+		audio.pitch = Random.Range(0.7f, 0.8f);
+
+		// audio de passos na agua
+	 	audioclip_temp = audioclip_agua;
+		
+ 	}
+ 	
+	// tocar audio
+	audio.PlayOneShot(audioclip_temp);
+	
+}
+
