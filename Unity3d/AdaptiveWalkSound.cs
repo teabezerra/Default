ce8597e543e4cf1bcb1032cb5aee25b940e19e57
teabezerra <thales.eab@gmail.com>
2016-06-26 18:32:20 -0300

    Segunda publicacao

---
+++ Unity3d/AdaptiveWalkSound.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Pequeno script para controle de audio de passos em diversos terrenos
+// Permite alternar dinamicamente o audio clip para cada tipo de terreno
+// Cria uma pequena variação de pitch, dando maior sensação de imersão de cena
+// A Vantagem da utilização deste método é o maior realismo pois o som nunca será exatamente o mesmo e só sera disparado no contato do solo com o pé do personagem. O que elimina problema de sincronismo com a velocidade da animação.
+// A desvantagem é o maior consumo de processamento.
+
+
+public class AdaptiveWalkSound : MonoBehaviour {
+
+    [SerializeField]
+    AudioClip audioclip_terra;          // AudioClip de passos na terra
+
+    [SerializeField]
+    AudioClip audioclip_agua;           // AudioClip de passos na água
+
+    private AudioClip audioclip_temp;   // AudioClip usado para referenciar o audio que sera tocado no momento.
+    private AudioSource _AudioSource;   // Referencia do audio source do objeto que receberá o script
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Instanciando
+        audioclip_temp = null;
+
+        // Capturando a referencia do AudioSource do objeto, evitando chamadas 
+        _AudioSource = GetComponent<AudioSource>();
+
+    }
+
+    void OnTriggerEnter(Collider tr)
+    {
+
+        if (tr.gameObject.name == "Terrain")
+        {
+
+            // Variação de sonoridade
+            _AudioSource.pitch = Random.Range(1.35f, 1.5f);
+
+            // audio de passos na terra
+            audioclip_temp = audioclip_terra;
+
+        }
+
+        else if (tr.gameObject.name == "water")
+        { // Caso a 
+
+            // Variação de sonoridade
+            _AudioSource.pitch = Random.Range(0.7f, 0.8f);
+
+            // audio de passos na agua
+            audioclip_temp = audioclip_agua;
+
+        }
+
+        // tocar audio
+        _AudioSource.PlayOneShot(audioclip_temp);
+
+    }
+
+}
--- Unity3d/AdaptiveWalkSound.js
+++ Unity3d/AdaptiveWalkSound.js
@@ -2,22 +2,25 @@
 
 // Pequeno script para controle de audio de passos em diversos terrenos
 // Permite alternar dinamicamente o audio clip para cada tipo de terreno
-// Cria uma peguena variação de pitch, dando maior sensação de imersão de cena
+// Cria uma pequena variação de pitch, dando maior sensação de imersão de cena
+// A Vantagem da utilização deste método é o maior realismo pois o som nunca será exatamente o mesmo e só sera disparado no contato do solo com o pé do personagem. O que elimina problema de sincronismo com a velocidade da animação.
+// A desvantagem é o maior consumo de processamento.
 
-
-var audioclip_terra: AudioClip;
-var audioclip_agua: AudioClip;
+var audioclip_terra : AudioClip; //Audio clip de passos na terra
+var audioclip_agua : AudioClip; //Audio clip de passos na água
 
 
 function OnTriggerEnter (tr : Collider)
 {
 
-	var audioclip_temp: AudioClip;
+    var audioclip_temp: AudioClip;
+    var _AudioSource: AudioSource;
+    _AudioSource = GetComponent.<AudioSource>();
 
 	if(tr.gameObject.name == "Terrain"){
 		
 		// Variação de sonoridade
-	 	audio.pitch = Random.Range(1.35f, 1.5f);
+	    _AudioSource.pitch = Random.Range(1.35f, 1.5f);
 		
 		// audio de passos na terra
 		audioclip_temp = audioclip_terra;
@@ -27,7 +30,7 @@ function OnTriggerEnter (tr : Collider)
 	else if(tr.gameObject.name == "water"){ // Caso a 
 		
 		// Variação de sonoridade
-		audio.pitch = Random.Range(0.7f, 0.8f);
+	    _AudioSource.pitch = Random.Range(0.7f, 0.8f);
 
 		// audio de passos na agua
 	 	audioclip_temp = audioclip_agua;
@@ -35,7 +38,7 @@ function OnTriggerEnter (tr : Collider)
  	}
  	
 	// tocar audio
-	audio.PlayOneShot(audioclip_temp);
+	_AudioSource.PlayOneShot(audioclip_temp);
 	
 }
 
